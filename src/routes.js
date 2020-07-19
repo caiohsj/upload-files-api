ea9a3f845f0d267b98935258c985f384a38312c1
Caio Henrique <caiocoxim10@gmail.com>
2020-07-19 16:41:59 -0300

    configurando multer e realizando upload de um arquivo

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const routes = express.Router();
+const multer = require('multer');
+const multerConfig = require('./config/multer');
 
-routes.get('/', (req, res) => {
-  res.send('ok');
+routes.get('/', (req, res) => {res.send()});
+routes.post('/', multer(multerConfig).single('file'), (req, res) => {
+  console.log(req.file);
+  res.send();
 });
 
 module.exports = routes;
